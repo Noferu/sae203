5d918d02851af8f2bb53d1019f5857bd9c9b8811
Noferu <nawfel.idaali@free.fr>
2024-05-20 23:27:09 +0200

    Grid visual started & minor fixes

--- assets/js/hover_cardtilt.js
+++ assets/js/hover_cardtilt.js
@@ -1,13 +1,20 @@
 (function($) {
     "use strict";
     $(".card").tilt({
-        maxTilt: 15,
+        maxTilt: 20,
         perspective: 1400,
         easing: "cubic-bezier(.03,.98,.52,.99)",
         speed: 1200,
         glare: true,
-        maxGlare: 0.2,
-        scale: 1.04
+        maxGlare: 0.3,
+        scale: 1.07,
     });
     console.log("Tilt.js initialized");
 }(jQuery));
+
+$(document).ready(function() {
+    const cards = document.querySelectorAll(".card-link");
+    cards.forEach((card, index) => {
+      card.style.animationDelay = `${index * 0.2}s`;
+    });
+  });
