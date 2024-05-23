78cf469abe9f9460e4a8d306c68de9018c4e6cb0
Noferu <nawfel.idaali@free.fr>
2024-05-23 17:54:08 +0200

    feat:favorites and cart

--- pages/checkout.php
+++ pages/checkout.php
@@ -1,90 +1,84 @@
 <?php
 
-// Inclusion des fichiers nécessaires
 include('../include/connexion.php');
 include('../include/data_access.php');
 include('../include/twig.php');
 
-// Initialisation de la connexion PDO et de Twig
 $pdo = connexion();
 $twig = init_twig();
 
 $categories = select_data($pdo, 'SELECT * FROM categories', [], true);
 $subcategories = select_data($pdo, 'SELECT * FROM subcategories', [], true);
 
-// Détermination de l'étape à partir de la requête GET ou par défaut à 'basket'
-$step = isset($_GET['step']) ? $_GET['step'] : 'basket';
+$step = isset($_GET['step']) ? $_GET['step'] : 'cart';
 
-// Démarrage de la session
-session_start();
-
-// Vérification si l'utilisateur est connecté, sinon redirection vers la page de connexion
-if (!isset($_SESSION['username'])) {
-    header("Location: userConnexion.php?action=login");
+if (!isset($_SESSION['user_id'])) {
+    header("Location: user_connexion.php?action=show_login_form");
     exit();
 }
 
-// Récupération de l'identifiant utilisateur depuis la session
-$user_id = $_SESSION['username'];
+$user_id = $_SESSION['user_id'];
 
-// Sélection de l'action à effectuer en fonction de l'étape
 switch ($step) {
     case 'favorites':
 
-        // Requête SQL pour récupérer les articles favoris de l'utilisateur
         $sql = "SELECT * 
                 FROM articles a 
-                JOIN favoris f ON a.article_id = f.article_id 
+                JOIN favorites f ON a.article_id = f.article_id 
                 JOIN users u ON f.user_id = u.user_id 
                 WHERE f.user_id = :identifiant";
 
-        $params = [':identifiant' => $user_id];
+        $articles = select_data($pdo, $sql, [':identifiant' => $user_id]);
 
-        // Exécution de la requête (les résultats ne sont pas utilisés pour le moment)
-        $articles = select_data($pdo, $sql, $params);
-
-        // Définition du template à utiliser
-        $template = 'cart_and_favorites.twig';
-        echo $twig->render($template, ['categories' => $categories,
-        'subcategories' => $subcategories]);
-        break;
-
-    case 'basket':
-
-        // Requête SQL pour récupérer les articles dans le panier de l'utilisateur
-        $sql = "SELECT * 
-                FROM articles a 
-                JOIN basket b ON a.article_id = b.article_id 
-                JOIN users u ON b.user_id = u.user_id 
-                WHERE b.user_id = :identifiant";
-
-        $params = [':identifiant' => $user_id];
-
-        // Exécution de la requête (les résultats ne sont pas utilisés pour le moment)
-        $articles = select_data($pdo, $sql, $params);
-
-        // Affichage du template 'basket.twig' avec des données vides
         echo $twig->render('cart_and_favorites.twig', [
             'categories' => $categories,
-            'subcategories' => $subcategories
+            'subcategories' => $subcategories,
+            'display' => 'favorites',
+            'favorite_items' => $articles
+        ]);
+        break;
+
+    case 'cart':
+
+        $sql = "SELECT * 
+                FROM articles a 
+                JOIN cart c ON a.article_id = c.article_id 
+                JOIN users u ON c.user_id = u.user_id 
+                WHERE c.user_id = :identifiant";
+
+        $params = [':identifiant' => $user_id];
+
+        $articles = select_data($pdo, $sql, $params);
+
+        $cart_total = 0;
+        foreach ($articles as $article) {
+            $cart_total += $article['price'] * $article['quantity'];
+        }
+
+        echo $twig->render('cart_and_favorites.twig', [
+            'categories' => $categories,
+            'subcategories' => $subcategories,
+            'display' => 'cart',
+            'cart_items' => $articles,
+            'cart_total' => $cart_total
         ]);
         break;
 
     case 'payment':
 
-        // Affichage du template 'payment_n_order.twig' pour l'étape de paiement avec des données vides
         echo $twig->render('payment_and_order.twig', [
             'categories' => $categories,
-            'subcategories' => $subcategories
+            'subcategories' => $subcategories,
+            'display' => 'payment'
         ]);
         break;
 
     case 'command_finished':
-        
-        // Affichage du template 'payment_n_order.twig' pour l'étape de commande terminée avec des données vides
+
         echo $twig->render('payment_and_order.twig', [
             'categories' => $categories,
-            'subcategories' => $subcategories
+            'subcategories' => $subcategories,
+            'display' => 'command_finished'
         ]);
         break;
 }
