4aab549a8f625d738742cfe4d148d35c2ab360b2
Noferu <nawfel.idaali@free.fr>
2024-05-24 11:56:02 +0200

    feat: multiples code access

--- include/security_gate.php
+++ include/security_gate.php
@@ -1,24 +1,37 @@
 <?php
 session_start();
 
-// Code d'accès requis
-$access_code = '789123';
+// Tableau des codes d'accès
+$access_codes = [
+    '789123', // Cazier, Robin, Lou et Marco
+    '456789', // Nolan
+    '123456'  // Personne
+];
 
 // Fonction pour enregistrer les tentatives
-function log_attempt($status) {
+function log_attempt($status, $access_code) {
     $ip = $_SERVER['REMOTE_ADDR'];
     $time = date('Y-m-d H:i:s');
-    file_put_contents('access_log.txt', "$time - $ip - $status\n", FILE_APPEND);
+    file_put_contents('access_log.txt', "$time - $ip $access_code - $status\n", FILE_APPEND);
 }
+
+// Initialiser le message d'erreur
+$error_msg = '';
 
 // Vérification du code
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['access_code']) && $_POST['access_code'] === $access_code) {
-        $_SESSION['access_granted'] = true;
-        log_attempt('SUCCESS');
+    if (!empty($_POST['access_code'])) {
+        $access_code = htmlspecialchars($_POST['access_code']);
+
+        if (in_array($access_code, $access_codes)) {
+            $_SESSION['access_granted'] = true;
+            log_attempt('SUCCESS', $access_code);
+        } else {
+            $error_msg = 'Code incorrect. Veuillez réessayer.';
+            log_attempt('FAIL', $access_code);
+        }
     } else {
-        $error_msg = 'Code incorrect. Veuillez réessayer.';
-        log_attempt('FAIL');
+        $error_msg = 'Remplissez le(s) champ(s).';
     }
 }
 
@@ -32,11 +45,13 @@ if (!isset($_SESSION['access_granted'])) {
     <body>
         <h1>Veuillez entrer le code d\'accès</h1>
         <form method="post" action="">
+            <br>
             <label for="access_code">Code d\'accès :</label>
             <input type="password" id="access_code" name="access_code" required>
+            <br>
             <button type="submit">Soumettre</button>
         </form>';
-    if (isset($error_msg)) {
+    if (!empty($error_msg)) {
         echo '<p style="color: red;">' . $error_msg . '</p>';
     }
     echo '</body>
