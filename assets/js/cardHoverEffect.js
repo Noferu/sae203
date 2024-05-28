dd6d102919421a5aa2db2bdd72bd7d6e8f7fb57c
Noferu <nawfel.idaali@free.fr>
2024-05-28 22:55:34 +0200

    feat: responsive view

--- assets/js/cardHoverEffect.js
+++ assets/js/cardHoverEffect.js
@@ -1,19 +1,37 @@
 (function($) {
-    "use strict";
-    $(".card").tilt({
-        maxTilt: 20,
-        perspective: 1400,
-        easing: "cubic-bezier(.03,.98,.52,.99)",
-        speed: 1200,
-        glare: true,
-        maxGlare: 0.3,
-        scale: 1.07,
-    });
-}(jQuery));
+  "use strict";
 
-$(document).ready(function() {
-    const cards = document.querySelectorAll(".card-link");
-    cards.forEach((card, index) => {
-      card.style.animationDelay = `${index * 0.2}s`;
-    });
+  function initializeTilt() {
+      $(".card").tilt({
+          maxTilt: 20,
+          perspective: 1400,
+          easing: "cubic-bezier(.03,.98,.52,.99)",
+          speed: 1200,
+          glare: true,
+          maxGlare: 0.3,
+          scale: 1.07,
+      });
+  }
+
+  function destroyTilt() {
+      $(".card").tilt.destroy.call($(".card"));
+  }
+
+  function checkTiltActivation() {
+      if (window.innerWidth <= 960) {
+          destroyTilt();
+      } else {
+          initializeTilt();
+      }
+  }
+
+  $(document).ready(function() {
+      const cards = document.querySelectorAll(".card-link");
+      cards.forEach((card, index) => {
+          card.style.animationDelay = `${index * 0.2}s`;
+      });
+      checkTiltActivation();
+      $(window).resize(checkTiltActivation);
   });
+
+}(jQuery));
