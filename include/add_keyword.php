ad794b5af57448b279c969e3a25c0bcb125639ec
Noferu <nawfel.idaali@free.fr>
2024-05-29 12:05:52 +0200

    fix

--- include/add_keyword.php
+++ include/add_keyword.php
@@ -22,26 +22,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (!$existLink) {
                 // Lien n'existe pas, l'ajouter
                 insert_data($pdo, 'articles_keywords', ['article_id' => $articleId, 'keyword_id' => $kwId]);
+                $data = ['updated_at' => date('Y-m-d H:i:s')];
+                $conditions = ['article_id' => $articleId];
+                update_data($pdo, 'articles', $data, $conditions);
                 echo json_encode(['success' => true, 'keyword_id' => $kwId]);
             } else {
                 // Lien déjà présent
-                echo json_encode(['success' => false, 'message' => 'Keyword already linked to this article']);
+                echo json_encode(['success' => false, 'message' => 'Le mot-clé est déjà lié à cet article.']);
             }
         } else {
             // Mot-clé n'existe pas, l'ajouter et créer le lien
             $kwId = insert_data($pdo, 'keywords', ['name' => $keyword]);
             if ($kwId) {
                 insert_data($pdo, 'articles_keywords', ['article_id' => $articleId, 'keyword_id' => $kwId]);
+                $data = ['updated_at' => date('Y-m-d H:i:s')];
+                $conditions = ['article_id' => $articleId];
+                update_data($pdo, 'articles', $data, $conditions);
                 echo json_encode(['success' => true, 'keyword_id' => $kwId]);
             } else {
-                echo json_encode(['success' => false, 'message' => 'Failed to add keyword']);
+                echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'ajout du mot-clé.']);
             }
         }
     } else {
         // Mot-clé vide
-        echo json_encode(['success' => false, 'message' => 'Keyword is empty']);
+        echo json_encode(['success' => false, 'message' => 'Le mot-clé renseigné est vide.']);
     }
 } else {
     // Méthode de requête non valide
-    echo json_encode(['success' => false, 'message' => 'Invalid request method']);
+    echo json_encode(['success' => false, 'message' => 'Erreur dans la requête.']);
 }
